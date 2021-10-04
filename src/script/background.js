223bcab14c63bec73ec2c43712ad9a412facde4b
ColourOfMagic <v.dalv747@gmail.com>
2021-10-04 22:26:12 +0300

    refactor!: delete useless float button from background script

--- src/script/background.js
+++ src/script/background.js
@@ -1,10 +1,4 @@
 // Set extension actions
-setTimeout(
-    function() {
-      let block = findOverviewPullRequestsBlock();
-      addFloatButton(block);
-    }, 100);
-
 chrome.runtime.onMessage.addListener(function(request) {
   if (request === 'copy_prs') {
     getLinkTextTypeAndCopyToClipboard();
@@ -32,25 +26,4 @@ function copyToClipboard(linkTextType) {
     document.execCommand('copy');
     document.removeEventListener('copy', listener);
   }
-}
-
-// Add button
-function addFloatButton(targetBlock) {
-  if (targetBlock) {
-    addTagToBaseElement(targetBlock);
-    addButton(targetBlock);
-  }
-}
-
-function addTagToBaseElement(targetBlock) {
-  targetBlock.className += ' check-pr-container';
-}
-
-function addButton(targetBlock) {
-  let input = document.createElement('input');
-  input.src = chrome.runtime.getURL('check-pr-copy.png');
-  input.type = 'image';
-  input.className = 'btn check-pr-button';
-  input.onclick = getLinkTextTypeAndCopyToClipboard;
-  targetBlock.appendChild(input);
 }
