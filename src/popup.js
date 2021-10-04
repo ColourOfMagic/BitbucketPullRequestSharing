edbc03dad9256cad1b7a23ec228ade7d3b590de1
ColourOfMagic <v.dalv747@gmail.com>
2021-10-04 19:55:44 +0300

    feat!(popup): move settings to popup
    Add settings to popup.
    Add copy button to popup.
    Delete settings from extension page.

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,41 @@
+const copyCommand = 'copy_prs';
+
+const copyBtn = document.getElementById('copy');
+const linkTextTypeElement = document.getElementById('linkTextType');
+const statusElement = document.getElementById('status');
+
+// Saves options to chrome.storage
+function save_options() {
+  const linkTextType = linkTextTypeElement.value;
+  chrome.storage.sync.set({favoriteLinkTextType: linkTextType}, () => setStatusMessage('Options saved.'));
+}
+
+// Restores select box and checkbox state using the preferences stored in chrome.storage
+function restore_options() {
+  // Use default value linkTextType = 'Pull Request Name'.
+  chrome.storage.sync.get(
+      {favoriteLinkTextType: 'pullRequestName'},
+      function(items) {linkTextTypeElement.value = items.favoriteLinkTextType;},
+  );
+}
+
+function setStatusMessage(message) {
+  statusElement.innerText = message;
+}
+
+// Events
+document.addEventListener('DOMContentLoaded', restore_options);
+copyBtn.addEventListener('click', sendCopyEvent);
+linkTextTypeElement.addEventListener('change', save_options);
+
+function sendCopyEvent() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, copyCommand, () => setStatusMessage('Copied !'));
+  });
+}
+
 chrome.commands.onCommand.addListener(function(command) {
-  if (command === 'copy_prs') {
+  if (command === copyCommand) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, command);
     });
